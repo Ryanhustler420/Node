22e6241b250b2215f4619698096e2e97243d779c
Gaurav Gupta <gouravgupta840@gmail.com>
2018-08-07 23:26:52 +0530

    played with _id.getTimestamp and object dstructuring

--- TodoApp with Rest API/playground/mongodb-connect.js
+++ TodoApp with Rest API/playground/mongodb-connect.js
@@ -1,4 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+//ObjectID is a constructor to make ObjectId on the fly;
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// //object destructuring
+// var user = {name:"Gaurav",age:20}
+// var {name} = user;
+// console.log(name);
 
 MongoClient.connect('mongodb://localhost:27017/todoApp',(err,db) => {
   if(err){
@@ -16,7 +26,7 @@ MongoClient.connect('mongodb://localhost:27017/todoApp',(err,db) => {
   //   console.log(JSON.stringify(result.ops,undefined,2));
   // })
 
-  //Insert new doc into Users (name,age,location)
+  Insert new doc into Users (name,age,location)
   db.collection('Users').insertOne({
     name:'Gaurav',
     age:20,
@@ -26,7 +36,8 @@ MongoClient.connect('mongodb://localhost:27017/todoApp',(err,db) => {
       return console.log('Unable to insert data into mongodb');
     }
     console.log('Successfully inserted one record');
-    console.log(JSON.stringify(result.ops,undefined,2));
+    // console.log(JSON.stringify(result.ops,undefined,2));
+    console.log(result.ops[0]._id.getTimestamp());
   })
   db.close();
 });
