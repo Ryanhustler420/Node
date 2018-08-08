20ce13610b72aefe3dc759a1159ebad0f33feea4
Gaurav Gupta <gouravgupta840@gmail.com>
2018-08-08 15:39:20 +0530

    Getting an individual Resource from mongoose and params validation

--- TodoApp with Rest API/server/db/mongoose.js
+++ TodoApp with Rest API/server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/todos');
+mongoose.connect('mongodb://localhost:27017/todos',{ useNewUrlParser: true });
 
 module.exports = {
   mongoose
--- TodoApp with Rest API/server/server.js
+++ TodoApp with Rest API/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { User } = require('./models/users');
@@ -36,6 +37,40 @@ app.get('/todos',(req,res) => {
   });
 })
 
+//fetching ID from url
+// GET /todos/0123456789
+
+app.get('/todos/:id',(req,res) => {
+    // res.send(req.params);
+    var _id = req.params.id;
+    // console.log(ObjectID.isValid(_id));
+    // console.log(_id);
+    if(!ObjectID.isValid(_id)){
+      return res.status(404).send({});
+    }
+    //validate id isValid
+    //404 - sand with empty body
+
+    Todo.findById(_id).then((todo) => {
+      console.log('reached');
+      if(!todo){
+        //if null send null body
+        return res.status(404).send({});
+      }
+      // if success
+      res.send({todo});
+    }).catch(e => {
+      res.status(400).send({})
+    });
+
+    //findById
+      //sucess case
+        //if todo send back
+        // if not todo  send- back 404 with empty body
+      //error =
+        // 400 status [send nothing back]
+
+});
 
 app.listen(3000,() => {
   console.log("server running");
