219cb31ecbfc8c918f4b44bec540965cff59e5b5
Gaurav Gupta <gouravgupta840@gmail.com>
2018-08-09 13:36:05 +0530

    chenged mongoose env variable

--- TodoApp with Rest API/server/db/mongoose.js
+++ TodoApp with Rest API/server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/todos',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todos',{ useNewUrlParser: true });
 
 module.exports = {
   mongoose
