12ebd87c7a2aff9d7ed0199f809c44d66b98dbfa
Gaurav Gupta <gouravgupta840@gmail.com>
2018-08-10 20:57:31 +0530

    added GET /users/me and authentication
    here we fetch the x-auth from users header and check with our database record for authentication

--- TodoApp with Rest API/server/server.js
+++ TodoApp with Rest API/server/server.js
@@ -8,7 +8,7 @@ const {ObjectID} = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { User } = require('./models/users');
 var { Todo } = require('./models/todos');
-
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -142,6 +142,10 @@ app.post('/users',(req,res) =>{
       res.status(400).send(e);
     });
   }
+});
+
+app.get('/users/me',authenticate ,(req,res) => {
+  res.send(req.user);
 });
 
 
