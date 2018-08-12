9d6d8a9877c5195c330b1d2c8fdcc31c0bcadf3d
Gaurav Gupta <gouravgupta840@gmail.com>
2018-08-12 10:23:15 +0530

    Hashing Password with Bcrypt before user saving using POST /users

--- TodoApp with Rest API/server/middleware/authenticate.js
+++ TodoApp with Rest API/server/middleware/authenticate.js
@@ -2,7 +2,7 @@ var {User} = require('../models/users');
 
 var authenticate = (req,res,next) => {
   var token = req.header('x-auth');
-
+  //this is static method and will not run automatically with document
   User.findByToken(token).then((user) => {
     if(!user){
       // res.status(401).send();
--- TodoApp with Rest API/server/models/users.js
+++ TodoApp with Rest API/server/models/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
@@ -67,6 +68,23 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.access':'auth'
   });
 };
+
+
+UserSchema.pre('save',function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,(err,salt) => {
+          bcrypt.hash(user.password,salt,(err,hash) => {
+            user.password = hash;
+            next();
+          })
+        });
+    }else{
+      next();
+    }
+
+});
 
 var User = mongoose.model('User',UserSchema);
 
