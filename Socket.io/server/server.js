2e4faf24c41796e1d384702fe53b3ef3aa10c081
Gaurav Gupta <gouravgupta840@gmail.com>
2018-08-14 23:31:56 +0530

    Add connect and disconnect event handlers
    modified package.json and change the project title because of npm package.
    we alson changes index.html and add an client side event emmitter for connection with server and in server.js we added an io.on connection event emmiter for client to connect!

--- Socket.io/server/server.js
+++ Socket.io/server/server.js
@@ -1,18 +1,28 @@
+const http = require('http');
 const path = require('path');
-const publicPath = path.join(__dirname,'../public');
-
 const express = require('express');
+const socketIO = require('socket.io');
 
-var app = express();
-
+const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.get('/',(req,res) => {
-  res.send('working')
+//register an event listener we listen for
+// diffrent event and do something with that
+
+
+//this is for server event
+io.on('connection',(socket) => {
+  console.log('new user connected!');
+  socket.on('disconnect',() => {
+    console.log('Disconnected from Client');
+  });
 });
 
-app.listen(port,() => {
+server.listen(port,() => {
   console.log(`server is up on port ${port}`);
-})
+});
