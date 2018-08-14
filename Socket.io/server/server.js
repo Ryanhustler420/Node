5c52c470ceec499b168652fe1b1ed66307394652
Gaurav Gupta <gouravgupta840@gmail.com>
2018-08-15 00:23:41 +0530

    Add newMessage and createMessage events
    created few more event emitter in both client and server file to make communication to happen!

--- Socket.io/server/server.js
+++ Socket.io/server/server.js
@@ -14,13 +14,32 @@ app.use(express.static(publicPath));
 //register an event listener we listen for
 // diffrent event and do something with that
 
+var emailStore = [{
+  from:'Gupta@example.com',
+  text:'Hey. What is going on.',
+  createdAt:123
+},{
+  from:'Mike@example.com',
+  text:'Gaurav Happy Birthday',
+  createdAt:123
+},{
+  from:'Jane@example.com',
+  text:'Hey.Gaurav are you there',
+  createdAt:123
+}];
 
 //this is for server event
 io.on('connection',(socket) => {
   console.log('new user connected!');
-  socket.on('disconnect',() => {
-    console.log('Disconnected from Client');
+
+  //Sender
+  socket.emit('newMessage',emailStore);
+
+  //Receiver
+  socket.on('createMessage',(createdMessage) => {
+    console.log('userCreatedMessage',createdMessage);
   });
+
 });
 
 server.listen(port,() => {
